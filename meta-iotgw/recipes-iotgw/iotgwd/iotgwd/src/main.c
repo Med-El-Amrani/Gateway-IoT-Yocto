dc1ba00cc9daa1274358797a1a7744e8c84e5b8d
amrani <moulaydrissmohammedelamrani@gmail.com>
2025-09-16 09:46:29 +0100

    write the main program

--- meta-iotgw/recipes-iotgw/iotgwd/iotgwd/src/main.c
+++ meta-iotgw/recipes-iotgw/iotgwd/iotgwd/src/main.c
@@ -1,18 +1,33 @@
+#include "app.h"
+#include "log.h"
+#include "version.h"
+#include <string.h>
 #include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdatomic.h>
 
-static atomic_int running = 1;
-static void on_sigint(int sig) { (void)sig; running = 0; }
+int main(int argc, char **argv){
+    const char *cfg = "/etc/iotgw.yaml";
+    const char *dir = "/etc/iotgwd";
 
-int main(void) {
-    signal(SIGINT, on_sigint);
-    fprintf(stderr, "[iotgwd] starting...\n");
-    while (running) {
-        fprintf(stderr, "[iotgwd] alive\n");
-        sleep(5);
+    for(int i=1;i<argc;i++){
+        if((strcmp(argv[i],"-c")==0 || strcmp(argv[i],"--config")==0) && i+1<argc) cfg = argv[++i];
+        else if(strcmp(argv[i],"--confdir")==0 && i+1<argc) dir = argv[++i];
+        else if(strcmp(argv[i],"--version")==0){ printf("iotgwd %s\n", IOTGWD_VERSION); return 0; }
+        else if(strcmp(argv[i],"-h")==0 || strcmp(argv[i],"--help")==0){
+            printf("Usage: %s [-c FILE|--config FILE] [--confdir DIR]\n", argv[0]);
+            return 0;
+        }
     }
-    fprintf(stderr, "[iotgwd] stopping.\n");
-    return 0;
+
+    log_init("iotgwd");
+
+    app_ctx_t app = {
+        .cfg_file = cfg,
+        .cfg_dir  = dir,
+        .stop     = 0,
+        .reload   = 0,
+    };
+
+    int rc = app_run(&app);
+    log_close();
+    return rc;
 }
